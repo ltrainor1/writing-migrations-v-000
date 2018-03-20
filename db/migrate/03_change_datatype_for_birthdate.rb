c573cad29bd77600ea4f3407aebc17bbe6ca8dbb
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-20 23:32:00 +0000

    automatically backed up by learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,8 +1,7 @@
 class ChangeColumn < ActiveRecord::Migration[5.1]
 
   def change(table_name, column_name, type)
-   change_column :table_name do |t|
-     column_name :type
+   change_column(table_name, column_name, type)
    end
- end
+
 end
